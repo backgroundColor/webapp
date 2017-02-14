d21c382150fbd72b2395280585c2fb89f0eba5e2
backgroundColor <625819969@qq.com>
2017-02-14 18:46:33 +0800

    map

--- src/components/MapBox/Map.js
+++ src/components/MapBox/Map.js
@@ -1,16 +1,76 @@
 import React from 'react'
-// import { BaiduMap } from 'react-baidu-map'
+import R from 'ramda'
+type Props = {
+  onClick: Function
+}
 export default class Map extends React.Component {
-
-  componentDidMount () {
-    this.createMap()
+  props: Props
+  constructor () {
+    super()
+    this.state = {
+      cities: []
+    }
+    this.getAllCities = this.getAllCities.bind(this)
+    this.showLocationInfo = this.showLocationInfo.bind(this)
   }
-  createMap () {
+  componentDidMount () {
+    this.getAllCities()
+    // this.createMap()
+  }
+  getAllCities () {
+    fetch(`${__TASK_URL__}projects/cities`)
+    .then((res) => res.status === 200 && res.json())
+    .then((json) => {
+      console.log(R.dropRepeats(json))
+      if (json) {
+        this.createMap(R.dropRepeats(json))
+      }
+    })
+  }
+  createMap (json) {
     const map = new BMap.Map('allmap')    // 创建Map实例
-    map.centerAndZoom(new BMap.Point(116.404, 39.915), 11)  // 初始化地图,设置中心点坐标和地图级别
+    map.centerAndZoom(new BMap.Point(116.404, 39.915), 5)  // 初始化地图,设置中心点坐标和地图级别
     map.addControl(new BMap.MapTypeControl())   //添加地图类型控件
     map.setCurrentCity('北京')          // 设置地图显示的城市 此项是必须设置的
     map.enableScrollWheelZoom(true)
+
+    const that = this
+    const bounds = map.getBounds()
+    const sw = bounds.getSouthWest()
+    const ne = bounds.getNorthEast()
+    const lngSpan = Math.abs(sw.lng - ne.lng)
+    const latSpan = Math.abs(ne.lat - sw.lat)
+    const addMarker = (point) => {
+      const marker = new BMap.Marker(point)
+      map.addOverlay(marker)
+      // marker.setAnimation(BMAP_ANIMATION_BOUNCE)
+    }
+    const myGeo = new BMap.Geocoder()
+    for (let i = 0, len = json.length; i < len; i++) {
+      myGeo.getPoint(json[i], function (point) {
+        if (point) {
+          const lng = point.lng
+          const lat = point.lat
+          const points = new BMap.Point(lng, lat)
+          const marker = new BMap.Marker(new BMap.Point(lng, lat))
+          const gc = new BMap.Geocoder()
+          addMarker(points)
+          marker.addEventListener('click', function (e) {
+            map.centerAndZoom(point, 15)
+            // console.log('click')
+            // const p = marker.getPosition()  // 获取marker的位置\
+            // const currentPoint = new BMap.Point(p.lng, p.lat)
+            gc.getLocation(e.point, function (rs) {
+              that.showLocationInfo(json[i])
+            })
+          })
+          map.addOverlay(marker)
+        }
+      }, json[i].name)
+    }
+  }
+  showLocationInfo (value) {
+    this.props.onClick(value)
   }
   render () {
     return (
--- src/components/MapBox/MapBox.js
+++ src/components/MapBox/MapBox.js
@@ -5,38 +5,54 @@ import Plot from '../Plot'
 import Map from './Map'
 export default class MapBox extends React.Component {
 
+  constructor () {
+    super()
+    this.state = {
+      data: []
+    }
+    this.mapClick = this.mapClick.bind(this)
+  }
+  mapClick (value) {
+    console.info(value)
+    fetch(`${__TASK_URL__}projects/city?city=${value}`)
+    .then((res) => res.status === 200 && res.json())
+    .then((json) => {
+      console.log(json)
+      this.setState({
+        data: json.body[0]
+      })
+    })
+  }
   render () {
+    const { data = [] } = this.state
     return (
       <div className={styles['map-box']}>
         <div className={styles['message']}>
           <div className={styles['map']}>
-            <Map />
+            <Map onClick={this.mapClick} />
           </div>
           <div className={styles['project-mess']}>
             <h4>testPro</h4>
             <div className={styles['img-box']}>
+            {
               <Carousel autoplay>
-                <div className={styles['img']}>
-                  <img height='200' src='http://img2.zol.com.cn/product/103_940x705/867/cesKh6vHhpmjE.jpg' />
-                </div>
-                <div className={styles['img']}>
-                  <img height='200' src='http://img2.zol.com.cn/product/103_940x705/867/cesKh6vHhpmjE.jpg' />
-                </div>
-                <div className={styles['img']}>
-                  <img height='200' src='http://img2.zol.com.cn/product/103_940x705/867/cesKh6vHhpmjE.jpg' />
-                </div>
-                <div className={styles['img']}>
-                  <img height='200' src='http://img2.zol.com.cn/product/103_940x705/867/cesKh6vHhpmjE.jpg' />
-                </div>
+                {
+                  data.urls && data.urls.map((item, index) => {
+                    return <div key={`img${index}`} className={styles['img']}>
+                      <img height='200' src={item} />
+                    </div>
+                  })
+                }
               </Carousel>
+              }
             </div>
             <div className={styles['project']}>
               <ul>
-                <li><span>省:</span><div>sss</div></li>
-                <li><span>市:</span><div>sss</div></li>
-                <li><span>项目:</span><div>sss</div></li>
-                <li><span>创建时间:</span><div>sss</div></li>
-                <li><span>描述:</span><div>ssssssssssssssss</div></li>
+                <li><span>省:</span><div>{data.province}</div></li>
+                <li><span>市:</span><div>{data.city}</div></li>
+                <li><span>项目:</span><div>{data.name}</div></li>
+                <li><span>创建时间:</span><div>{data.createTime}</div></li>
+                <li><span>描述:</span><div>{data.desc}</div></li>
               </ul>
             </div>
           </div>
